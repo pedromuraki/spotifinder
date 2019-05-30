e9ca0b0141fbf657034608f2200eb106b023a687
Pedro Muraki <pedromuraki@gmail.com>
2019-05-30 18:06:22 -0300

    Created localStorage for the token

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -17,25 +17,30 @@ class App extends React.Component {
     super()
 
     this.state = {
-      token: null
+      token: localStorage.getItem('token') || null
     }
   }
 
   componentDidMount() {
-    // If not returning from auth (based on url hash), redirect to it
-    if (history.location.hash.slice(0, 13) !== '#access_token') {
-      window.location.href = AUTH_URL
-    }
-
-    // Set the 'token' state using the returning auth url as ref
-    this.setState({
-      token: history.location.hash
+    // If coming from auth url...
+    if (history.location.hash.slice(0, 13) === '#access_token') {
+      // Set the 'token' state and localStorage using the returning auth url as ref
+      const token = history.location.hash
         .replace(/#access_token=/, '')
         .replace(/&token_type=Bearer&expires_in=3600/, '')
-    })
 
-    // Push to main route to clear the returning auth url
-    history.push('/')
+      this.setState({ token: token })
+
+      localStorage.setItem('token', token)
+
+      // Push to main route to clear the returning auth url
+      history.push('/')
+
+      return
+    }
+
+    // If token does not exist, send to auth url
+    if (!this.state.token) window.location.href = AUTH_URL
   }
 
   render() {
